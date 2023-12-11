523f17552f437311f7cbb4dc9a21e3a4bd243b98
Nextross <70912486+Nextross@users.noreply.github.com>
2023-12-11 21:31:40 +0100

    Added option to delete/keep output files, reformated code

--- tester/tester.sh
+++ tester/tester.sh
@@ -5,8 +5,6 @@ function help() {
 }
 
 function compare() {
-  # files must be in correct format "*_in"
-  # check if path is directory
    for FILE_IN in "$2"/*_in; do
      cat "$FILE_IN"
      echo
@@ -14,14 +12,11 @@ function compare() {
 }
 
 function generate_output () {
-
-  #check if binary exists
   if [ ! -e "$1" ]; then
     echo "File doesn't exist. Did you write it correctly?"
     exit
   fi
 
-  #check if data input is dir or empty
   if [ ! -d "$2" ]; then
     echo "Directory doesn't exist. Did you write it correctly?"
     exit
@@ -44,33 +39,35 @@ function generate_output () {
 }
 
 function delete_output() {
-  echo sds
+  rm -rd "$1"
 }
 
-PATH_TO_BIN=a.out
-PATH_TO_INPUT="$PWD"/sample/CZE
-PATH_TO_OUTPUT="$PWD"/output
-KEEP=0
+function get_params() {
+  while getopts ":ht:s:o:k" option; do
+    case "$option" in
+      h) help; exit;;
+      t) PATH_TO_BIN="$OPTARG";;
+      s) PATH_TO_INPUT="$PWD/$OPTARG";;
+      o) PATH_TO_OUTPUT="$PWD/$OPTARG";;
+      k) KEEP=1;;
+      \?) echo "Not a flag, exiting..."; exit;;
+    esac
+  done
+  shift $((OPTIND - 1))
+}
 
+function main() {
+  PATH_TO_BIN=a.out
+  PATH_TO_INPUT="$PWD"/sample/CZE
+  PATH_TO_OUTPUT="$PWD"/output
+  KEEP=0
 
+  get_params "$1"
+  generate_output "$PATH_TO_BIN" "$PATH_TO_INPUT" "$PATH_TO_OUTPUT"
 
-while getopts ":ht:s:o:k:" option; do
-  case "$option" in
-    h) help; exit;;
-    t) PATH_TO_BIN="$OPTARG";;
-    s) PATH_TO_INPUT="$PWD/$OPTARG";;
-    o) PATH_TO_OUTPUT="$PWD/$OPTARG";;
-    k) PATH_TO_OUTPUT="$OPTARG"; KEEP=1;;
-    \?) echo "Not a flag, exiting..."; exit;;
-  esac
-done
+  if [ "$KEEP" -eq 0 ]; then
+    delete_output "$PATH_TO_OUTPUT"
+  fi
+}
 
-shift $((OPTIND - 1))
-
-generate_output "$PATH_TO_BIN" "$PATH_TO_INPUT" "$PATH_TO_OUTPUT"
-
-if [ "$KEEP" -eq 0 ]; then
-  delete_output "$PATH_TO_OUTPUT"
-fi
-
-
+main "$@"
