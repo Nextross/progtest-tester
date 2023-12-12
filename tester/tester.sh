2a7934589ccd5d54d9457da7f2610ae2d32cfcb0
Nextross <70912486+Nextross@users.noreply.github.com>
2023-12-12 13:50:55 +0100

    Fixed generate_output(), improved compare function

--- tester/tester.sh
+++ tester/tester.sh
@@ -5,11 +5,21 @@ function help() {
 }
 
 function compare() {
+  local RED='\033[0;31m' GREEN='\033[0;32m' NO_COLOR='\033[0m'
   local i=0
-  for REF_FILE in "$1"/*out*; do
+
+  for REF_FILE in "$1"/*out.txt; do
     OUT_FILE="$2"/"$i"_out.txt
-    diff "$REF_FILE" "$OUT_FILE" > tmp
-    cat tmp
+    diff  "$REF_FILE" "$OUT_FILE" > tmp
+    REF_FILE="$(echo "$REF_FILE" | grep -o '[0-9]*_out.txt')"
+
+    if [ -s tmp ]; then
+      printf "[${RED}FAILED${NO_COLOR}]: %s\n" "$REF_FILE"
+      tail -n +2 tmp
+      else
+        printf "[${GREEN}OK${NO_COLOR}]: %s\n" "$REF_FILE"
+    fi
+
     i=$((i + 1))
   done
   rm tmp
@@ -17,17 +27,17 @@ function compare() {
 
 function generate_output () {
   if [ ! -e "$1" ]; then
-    echo "File doesn't exist. Did you write it correctly?"
+    echo "File $1 doesn't exist. Did you write it correctly?"
     exit
   fi
 
   if [ ! -d "$2" ]; then
-    echo "Directory doesn't exist. Did you write it correctly?"
+    echo "Directory $2 doesn't exist. Did you write it correctly?"
     exit
   fi
 
   if [ ! "$(ls -A "$2")" ]; then
-    echo "Directory is empty. Did you write it correctly?"
+    echo "Directory $2 is empty. Did you write it correctly?"
     exit
   fi
 
@@ -36,8 +46,9 @@ function generate_output () {
   fi
 
   local i=0
-  for IN_FILE in "$2"/*in*; do
-    ./"$1" < "$IN_FILE" > "$3"/"$i"_out.txt
+  for IN_FILE in "$2"/*_in.txt; do
+    OUT_FILE="$3"/"$i"_out.txt
+    ./"$1" < "$IN_FILE" > "$OUT_FILE"
     i=$((i + 1))
   done
 }
